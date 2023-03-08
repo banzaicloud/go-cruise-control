581429fadf9da84f2e811757d9a041b7bcb9a8f1
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2023-03-08 22:17:42 +0100

    refactor: reduce compose timeout for deploy

--- makefile.d/50-deploy.mk
+++ makefile.d/50-deploy.mk
@@ -3,14 +3,14 @@
 DOCKER_COMPOSE_PROJECT_NAME = go-cruise-control
 DEPLOY_DIR ?= $(PROJECT_DIR)/deploy
 COMPOSE_PROFILES ?=
-DOCKER_COMPOSE_TIMEOUT ?= 120
+DOCKER_COMPOSE_TIMEOUT ?= 60
 
 export COMPOSE_PROFILES
 
 .PHONY: start
 start: ## Spin up local development environment
 	$(info *** Spinning up local development environment...)
-	docker compose \
+	@docker compose \
 		--project-name "$(DOCKER_COMPOSE_PROJECT_NAME)" \
 		--project-directory "$(DEPLOY_DIR)" \
 		up \
